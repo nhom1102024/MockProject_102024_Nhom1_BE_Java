cbaedab6dc19de77ece4061283d43a341618be5f
thuyhang2401 <truongthuyhanghhn@gmail.com>
2024-10-20 18:01:23 +0700

    Update api "Get list candidates"

--- MockProject/src/main/java/com/be/service/candidate/ICandidateService.java
+++ MockProject/src/main/java/com/be/service/candidate/ICandidateService.java
@@ -1,16 +1,13 @@
-package com.be.service;
+package com.be.service.candidate;
 
-import java.util.List;
 import java.util.Optional;
 
-import com.be.dto.CandidateStatusDTO;
-import com.be.dto.CandidateUpdateDTO;
+import com.be.dto.candidate.CandidateStatusDTO;
+import com.be.dto.candidate.CandidateUpdateDTO;
 import com.be.model.Candidate;
 
 public interface ICandidateService {
     Optional<Candidate> getCandidateByID(Integer candidatesId);
-
-    List<Candidate> searchCandidates(String query, Integer page, Integer limit);
 
     void updateCandidateStatus(Integer candidatesId, CandidateStatusDTO candidateStatusDTO);
 
