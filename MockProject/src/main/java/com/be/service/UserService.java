d43a60ed26460f46a1c8cd0578dae2cdbfe02073
LXUAcjQY3pcX99Y <hennguyen862@gmail.com>
2024-10-21 09:24:17 +0700

    Add Api Edit Profile , Api View Payment history

---
+++ MockProject/src/main/java/com/be/service/UserService.java
@@ -0,0 +1,32 @@
+package com.be.service;
+
+
+import com.be.model.User;
+import com.be.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User updateProfile(Long id, User updatedUser) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            User existingUser = userOptional.get();
+            existingUser.setUsername(updatedUser.getUsername());
+            existingUser.setEmail(updatedUser.getEmail());
+            existingUser.setFirstName(updatedUser.getFirstName());
+            existingUser.setLastName(updatedUser.getLastName());
+            existingUser.setPassword(updatedUser.getPassword());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+}
